c739f6c6c87656cc4a88c147f6ac24d49a5ad3b6
romanvillagran <romanvillagran386@gmail.com>
2025-04-11 19:23:11 -0700

    added the product.cs file and also added required things like name

---
+++ COMP003B.Assignment4/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment4.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Range(0.01, 1000)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string ImageName { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+    }
+}
