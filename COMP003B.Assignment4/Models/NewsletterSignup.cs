67326641e5abb38bff4c01b8b57f0cea69c2437f
romanvillagran <romanvillagran386@gmail.com>
2025-04-11 19:34:48 -0700

    added a newslettersignup.cs that ask for name and email for a newsletter

---
+++ COMP003B.Assignment4/Models/NewsletterSignup.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment4.Models
+{
+    public class NewsletterSignup
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "Receive Promotions")]
+        public bool ReceivePromotions { get; set; }
+    }
+}
