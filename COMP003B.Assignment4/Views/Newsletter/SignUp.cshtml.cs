9e3721597db77d9bffb00a16878c6784f215ce58
romanvillagran <romanvillagran386@gmail.com>
2025-04-11 21:41:00 -0700

    added signup.cshtml

---
+++ COMP003B.Assignment4/Views/Newsletter/SignUp.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace COMP003B.Assignment4.Views.Newsletter
+{
+    public class SignUpModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
