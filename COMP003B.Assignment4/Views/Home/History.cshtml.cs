c37483ceb09553a71ca08d7b4e3a36b78cd6875c
romanvillagran <romanvillagran386@gmail.com>
2025-04-11 23:32:39 -0700

    added history file

---
+++ COMP003B.Assignment4/Views/Home/History.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace COMP003B.Assignment4.Views.Home
+{
+    public class HistoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
