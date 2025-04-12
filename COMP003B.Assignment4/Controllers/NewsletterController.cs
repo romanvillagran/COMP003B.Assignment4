2c2957926a89723fe0575fbaf4a5efd2fccada01
romanvillagran <romanvillagran386@gmail.com>
2025-04-11 20:56:45 -0700

    added content in the news letter controller

--- COMP003B.Assignment4/Controllers/NewsletterController.cs
+++ COMP003B.Assignment4/Controllers/NewsletterController.cs
@@ -4,6 +4,29 @@ namespace COMP003B.Assignment4.Controllers
 {
     public class NewsletterController : Controller
     {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
 
+        [HttpPost]
+        public IActionResult SignUp(Newsletter signup)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("ThankYou");
+            }
+            return View(signup);
+        }
+
+        public IActionResult ThankYou()
+        {
+            return View();
+        }
+    }
+
+    public class Newsletter
+    {
     }
 }
+
